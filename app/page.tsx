9595003597039474a066174d4b0185418b51e661
emrosesajib <emrose8981@gmail.com>
2026-08-13 22:29:58 +0600

    brushup

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import { Location } from "@/components/location";
 import { MenuSection } from "@/components/menu-section";
 import { Navbar } from "@/components/navbar";
 import { OurStory } from "@/components/our-story";
-import Reservation from "@/components/reservation";
 import { Reviews } from "@/components/reviews";
 import { useEffect, useState } from "react";
 
@@ -26,7 +25,7 @@ export default function Home() {
       <MenuSection />
       {/* <ChefSpecials /> */}
       <Gallery />
-      <Reservation />
+      {/* <Reservation /> */}
       <Reviews />
       <Location />
       <Footer />
